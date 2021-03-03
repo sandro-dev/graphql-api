b7a5a5c0ce863b86ed6b167f3d46df9e3653a17d
Sandro Santos <sandro.dev@gmail.com>
2021-03-03 16:08:17 -0300

    feat: mongoose and dotenv library and config

--- src/startServer.js
+++ src/startServer.js
@@ -1,6 +1,18 @@
+import 'dotenv/config';
+
+import mongoose from 'mongoose';
+
 import { ApolloServer } from 'apollo-server';
 
 function startServer({ typeDefs, resolvers }) {
+
+  const url = `mongodb://${process.env.MONGO_URL}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`;
+  mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+
   const server = new ApolloServer({typeDefs, resolvers});
   server.listen().then(({ url }) => console.log(`✅ Server started at ${url} ⚫⚫⚫`));
 
